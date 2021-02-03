8a7264f87ddef1c36b17d83c5766817e5ae53045
IncheonPark <dnehd8@gmail.com>
2021-02-03 13:16:33 +0900

    to s091. 03-Feb-21

---
+++ my_java200_chap04/src/kr/co/infopub/chapter/s091/BillboardTimeMain.java
@@ -0,0 +1,51 @@
+//이번 주 빌보드 차트 기준 날짜 찾기
+
+package kr.co.infopub.chapter.s091;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class BillboardTimeMain {
+	
+	  public static void main(String[] args) {
+		  
+		  String newUrls = "https://www.billboard.com/charts/hot-100/";
+		  HttpGet request = new HttpGet(newUrls); // HttpClient를 이용하여
+		  CloseableHttpClient httpClient = null;
+		  CloseableHttpResponse response = null;
+		  Document doc = null;
+		  
+		  try {
+			  httpClient = HttpClients.createDefault(); //웹에 연결
+			  response = httpClient.execute(request); //결과 얻기
+			  HttpEntity entity = response.getEntity();
+			  StringBuffer sb = new StringBuffer(); //5000줄 정도 저장
+			  
+			  if (entity != null) { //결과가 있다면 결과에서 한 줄씩
+				  String result = EntityUtils.toString(entity); //toString 실행
+				  sb.append(result); //버퍼에 한 줄씩 저장
+			  }
+			  
+			  doc = Jsoup.parse(sb.toString()); //전체 내용을 HTML 파싱
+			  String id = "charts";
+			  // <div id="charts" data-chart-date="yyyy-MM-dd"
+			  Element eid = doc.getElementById(id); // id가 "charts"인 요소를 찾음
+			  String sdate = eid.attr("data-chart-date"); //속성 값
+			  System.out.println("이번 빌보드 차트 날짜 : "+sdate.trim());
+			  
+		  } catch (IOException e) {
+			  System.out.println(e);
+		  }
+		  
+	  }
+
+}
